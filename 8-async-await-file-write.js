e0a52201946ca8a6e84a2e8781df2fa607a10bf3
layesh <layesh.iut@gmail.com>
2024-09-26 23:28:49 +0600

    added async await example code

--- 8-async-await-file-write.js
+++ 8-async-await-file-write.js
@@ -2,10 +2,10 @@ const { readFile, writeFile } = require('fs').promises;
 
 const mergeFile = async () => {
     try {
-        const fileContent1 = await readFile('./1.txt', 'utf8')
-        const fileContent2 = await readFile('./2.txt', 'utf8')
+        const fileContent1 = await readFile('./contents/1.txt', 'utf8')
+        const fileContent2 = await readFile('./contents/2.txt', 'utf8')
 
-        await writeFile('combined-async-await.txt', fileContent1 + fileContent2, { flag: a })
+        await writeFile('./contents/combined-async-await.txt', fileContent1 + fileContent2, { flag: 'a' })
     } catch (error) {
         console.log(error)
     }
