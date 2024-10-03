5c2914237be5fc414f740380430ce0f2e130ffc8
layesh <layesh.iut@gmail.com>
2024-10-03 12:41:46 +0600

    read file by stream

--- 11-create-big-file.js
+++ 11-create-big-file.js
@@ -1,5 +1,5 @@
 const { writeFileSync } = require('fs')
 
-for (let i = 0; i <= 1000; i++) {
+for (let i = 0; i <= 10000; i++) {
     writeFileSync('./contents/bigfile.txt', `hello world ${i}\n`, { flag: 'a' })
 }
